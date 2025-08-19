011b4d8c579de012ad741a3be0e4a2f2830fb186
Ronny B Good <ronny.bruhin+bbc@hotmail.com>
2025-08-19 10:16:26 +0200

    button fixed | css applied

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-button',
+  selector: '[appButton]', //button.button  button[appButton] .button
   standalone: true,
   imports: [],
   templateUrl: './button.component.html',
