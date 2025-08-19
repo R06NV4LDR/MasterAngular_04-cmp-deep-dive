19960741fcc05f74dd6a8e7a909ea0c56d74763e
Ronny B Good <ronny.bruhin+bbc@hotmail.com>
2025-08-19 16:25:28 +0200

    new ticket submitting works

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
-import { ButtonComponent } from '../../../shared/button/button.component';
+import { Component } from "@angular/core";
+import { ButtonComponent } from "../../../shared/button/button.component";
 import { ControlComponent } from "../../../shared/control/control.component";
+import { FormsModule } from "@angular/forms";
 
 @Component({
-  selector: 'app-new-ticket',
+  selector: "app-new-ticket",
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
-  templateUrl: './new-ticket.component.html',
-  styleUrl: './new-ticket.component.css'
+  imports: [ButtonComponent, ControlComponent, FormsModule],
+  templateUrl: "./new-ticket.component.html",
+  styleUrl: "./new-ticket.component.css",
 })
 export class NewTicketComponent {
-
+  onSubmit(titleElement: HTMLInputElement) {
+    console.log("SUBMITTED");
+    console.log(titleElement);
+    const enteredTitle = titleElement.value;
+  }
 }
