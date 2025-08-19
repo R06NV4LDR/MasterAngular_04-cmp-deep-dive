1f141ca622460143e26f20c80204f57e21fd04eb
Ronny B Good <ronny.bruhin+bbc@hotmail.com>
2025-08-19 10:26:42 +0200

    ng-content for button added

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-server-status',
+  selector: "app-server-status",
   standalone: true,
   imports: [],
-  templateUrl: './server-status.component.html',
-  styleUrl: './server-status.component.css'
+  templateUrl: "./server-status.component.html",
+  styleUrl: "./server-status.component.css",
 })
 export class ServerStatusComponent {
-  currentStatus = 'online';
+  currentStatus: "online" | "offline" | "unknown" = "offline";
 
+  constructor() {}
+
+  ngOnInit() {
+    setInterval(() => {
+      const rnd = Math.random(); // 0 - 0.999999999999999
+
+      if (rnd < 0.5) {
+        this.currentStatus = "online";
+      } else if (rnd < 0.9) {
+        this.currentStatus = "offline";
+      } else {
+        this.currentStatus = "unknown";
+      }
+    }, 5000);
+  }
 }
